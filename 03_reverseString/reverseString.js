d32c82f3e9a4286de91432910944d804bc2c3e09
shihabfaruk <sfaruk3131p@gmail.com>
2023-09-30 08:38:56 -0400

    Finished reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const wordArr = word.split("");
+    let reversedStr = "";
+    for (let i = wordArr.length-1; i >= 0; i--) {
+        reversedStr = reversedStr + wordArr[i];
+    }
+    return reversedStr;
 };
 
 // Do not edit below this line
