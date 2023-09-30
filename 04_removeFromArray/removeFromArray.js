2e287f5aba0c6e13d4d967dab62f0627d8d35878
shihabfaruk <sfaruk3131p@gmail.com>
2023-09-30 09:14:12 -0400

    Finished removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...manyMoreArgs) {
+    const newArr = [];
 
+    
+    for (let i = 0; i < arr.length; i++) {
+        let elm = arr[i];
+        if (!manyMoreArgs.includes(elm)) {
+            newArr.push(elm);
+        }
+    }
+    
+    return newArr;
 };
 
 // Do not edit below this line
