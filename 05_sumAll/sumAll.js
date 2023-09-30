c0c2b1347fa2fb6312f600216ab8c1402960ff68
shihabfaruk <sfaruk3131p@gmail.com>
2023-09-30 09:52:46 -0400

    Finished sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof start !== 'number' || typeof end !== 'number') {
+        return "ERROR";
+    }
+    
+    if ((start < 0 || end < 0)) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    if (end < start) {
+        let temp = end;
+        end = start;
+        start = temp;
+    }
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+
+//algorithm
+//Create a variable for sum
+//loop through interval start to end. Add the number at each iteration to sum.
+//return sum
